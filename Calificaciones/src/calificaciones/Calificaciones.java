a4b1cbc59712cdffae1752985ec67a1e97fe10c6
AngelEduardo-prog <72528299+AngelEduardo-prog@users.noreply.github.com>
2021-01-18 20:52:01 -0600

    Add files via upload

---
+++ Calificaciones/src/calificaciones/Calificaciones.java
@@ -0,0 +1,22 @@
+
+package calificaciones;
+import java.util.Scanner;
+/**
+ *
+ * @author ramir
+ */
+public class Calificaciones {
+      public String Calificacion;
+      public String Promediof;
+      public String cal;
+      public String cam;
+      public String aprov;
+      public String repro;
+      public String sup;
+      
+         
+        
+        
+    }
+    
+
---
+++ Calificaciones/src/calificaciones/Principal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calificaciones;
+
+/**
+ *
+ * @author ramir
+ */
+public class Principal {
+    public static void main (String[] args) {
+      int Calificaciones;
+        
+     String arraycalificaciones[] = new String[10];
+        arraycalificaciones[0] = "10";
+        arraycalificaciones[1] = "10";
+        arraycalificaciones[2] = "10";
+        arraycalificaciones[3] = "10";
+        arraycalificaciones[4] = "10";
+        arraycalificaciones[5] = "10";
+        arraycalificaciones[6] = "10";
+        arraycalificaciones[7] = "10";
+        arraycalificaciones[8] = "10";
+        arraycalificaciones[9] = "10";
+        arraycalificaciones[10] = "10";
+        for (int i = 0;i<arraycalificaciones.length;i++)
+        {
+           System.out.println(arraycalificaciones.length); 
+           
+          
+        };
+        System.out.println("Ingresa la calificacion");
+         
+                 
+    }
+
+    @Override
+    public String toString() {
+        return calificaciones.toString(); 
+        System.out.println("Calificaciones"+ String arraycalificaciones);
+    };
+        
+    
